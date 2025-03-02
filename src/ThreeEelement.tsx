07f1111d2db8100f091464806315e474e926808f
경명우 <kmsy121@naver.com>
2025-03-02 14:50:07 +0900

    world, local 좌표계 구현

--- src/ThreeEelement.tsx
+++ src/ThreeEelement.tsx
@@ -6,6 +6,7 @@ export default function ThreeElement() {
   const { size, gl, scene, camera } = useThree();
 
   const boxRef = useRef<Three.Mesh>(null);
+  const groupRef = useRef<Three.Group>(null);
 
   useFrame((state, delta) => {
     console.log(state);
@@ -14,11 +15,20 @@ export default function ThreeElement() {
       boxRef.current.rotation.x += delta;
     }
     //scene.position.x += 0.01;
+    groupRef.current.rotation.x += delta;
   });
   return (
     <>
       <directionalLight position={[5, 5, 5]}> </directionalLight>
-      <group position={[1, 0, 0]}>
+      <group
+        ref={groupRef}
+        position={[4, 0, 0]}
+        rotation={[
+          Three.MathUtils.degToRad(30),
+          Three.MathUtils.degToRad(30),
+          Three.MathUtils.degToRad(0),
+        ]}
+      >
         <mesh
           ref={boxRef}
           rotation={[
@@ -27,10 +37,46 @@ export default function ThreeElement() {
             0,
           ]}
           position={[2, 0, 0]}
+          position-x={[2]}
+          position-y={[3]}
           scale={[6, 1, 1]}
+          rotation-z={[Three.MathUtils.degToRad(30)]}
         >
           <boxGeometry></boxGeometry>
           <meshStandardMaterial color="red"></meshStandardMaterial>
+          <axesHelper args={[3]} />
+        </mesh>
+
+        <mesh
+          rotation={[
+            Three.MathUtils.degToRad(45),
+            Three.MathUtils.degToRad(45),
+            0,
+          ]}
+          position={[2, 0, 0]}
+          position-x={[2]}
+          position-y={[3]}
+          scale={[6, 1, 1]}
+          rotation-z={[Three.MathUtils.degToRad(30)]}
+        >
+          <boxGeometry></boxGeometry>
+          <meshStandardMaterial color="blue"></meshStandardMaterial>
+        </mesh>
+
+        <mesh
+          rotation={[
+            Three.MathUtils.degToRad(45),
+            Three.MathUtils.degToRad(45),
+            0,
+          ]}
+          position={[2, 0, 0]}
+          position-x={[1]}
+          position-y={[3]}
+          scale={[6, 1, 1]}
+          rotation-z={[Three.MathUtils.degToRad(30)]}
+        >
+          <boxGeometry></boxGeometry>
+          <meshStandardMaterial color="green"></meshStandardMaterial>
         </mesh>
       </group>
     </>
